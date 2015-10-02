abd9349eefdbb0bf620b80d3267a9cef2450e768
benn0r <benjamin@benn0r.ch>
2015-10-02 15:02:47 +0200

    Add simple writer

--- src/Hayloft/Log/Writer/Elastic.php
+++ src/Hayloft/Log/Writer/Elastic.php
@@ -9,12 +9,44 @@
 
 namespace Hayloft\Log\Writer;
 
+use Elastica\Document;
 use Zend\Log\Writer\AbstractWriter;
+use Elastica\Client;
+use Elastica\Index;
 
-class Elastic extends AbstractWriter
+final class Elastic extends AbstractWriter
 {
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @var Index
+     */
+    private $index;
+
+    /**
+     * @param array $options
+     */
+    public function __construct($options = [])
+    {
+        parent::__construct($options);
+        $this->client = new Client($options);
+
+        if (!isset($options['index'])) {
+            $options['index'] = 'log';
+        }
+        $this->index = $this->client->getIndex($options['index']);
+    }
+
+    /**
+     * @param array $event
+     */
     protected function doWrite(array $event)
     {
-        echo '11';
+        $document = new Document('', $event);
+        $type = $this->index->getType(strtolower($event['priorityName']));
+        $type->addDocument($document);
     }
 }
